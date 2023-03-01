8e9667a413f24fd0c667e5d2ebde9871a6ba5bf3
Oleg <olegkatyskin@gmail.com>
2023-03-01 21:14:47 +0300

    sss

---
+++ Mario.py
@@ -0,0 +1,165 @@
+import pygame
+import sys
+import os
+
+FPS = 50
+SIZE = WIDTH, HEIGHT = 550, 550
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def get_file_name():
+    try:
+        with open(f"data/maps/{input()}") as f:
+            return f.name
+    except FileNotFoundError:
+        print('Нет такого файла')
+        sys.exit()
+
+
+def load_level(filename):
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    max_width = max(map(len, level_map))
+
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры"]
+
+    fon = pygame.transform.scale(load_image('box.png'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, True, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('mar.png')
+
+tile_width = tile_height = 50
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+    def update(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_DOWN]:
+            self.rect = self.rect.move(0, 50)
+        if pressed[pygame.K_UP]:
+            self.rect = self.rect.move(0, -50)
+        if pressed[pygame.K_RIGHT]:
+            self.rect = self.rect.move(50, 0)
+        if pressed[pygame.K_LEFT]:
+            self.rect = self.rect.move(-50, 0)
+
+
+class Camera:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+        print(self.dx, self.dy)
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+if __name__ == '__main__':
+    level = load_level(get_file_name())
+    pygame.init()
+    pygame.display.set_caption('Mario')
+    screen = pygame.display.set_mode(SIZE)
+    player, level_x, level_y = generate_level(level)
+    screen.fill('Black')
+
+    clock = pygame.time.Clock()
+    start_screen()
+    camera = Camera()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            all_sprites.update()
+
+        camera.update(player)
+        for sprite in all_sprites:
+            camera.apply(sprite)
+
+        screen.fill('Black')
+        tiles_group.draw(screen)
+        player_group.draw(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
